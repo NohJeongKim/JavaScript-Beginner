49cf6b3cc841d57514faa45550e2992995c27ace
KIM NOH JEONG <97859517+NohJeongKim@users.noreply.github.com>
2022-02-09 22:35:36 +0900

    Add files via upload

---
+++ 6비교연산자,조건문.js
@@ -0,0 +1,48 @@
+// 비교 연산자와 조건문
+console.log(10 > 5, 10 == 5, 10 != 5);
+// true false true
+const a = 1;
+const b = "1";
+console.log(a == b); // true
+console.log(a === b); // false
+console.log(a != b); // false
+// 타입은 고려를 안 하기 때문에 1와 "1"이 같다고 보기 때문에 false이다.
+console.log(a !== b); // true
+// 타입도 고려하기 때문에 1와 "1"이 다르다고 보기 때문에 true이다.
+
+const name1 = "TOM";
+const isAdult = false; // 성인이 아님을 의미한다.
+
+if (name1 === "TOM" && isAdult == true) {
+  // 이름이 TOM이고 성인인 경우 > 통과
+  console.log("PASS");
+} // 값이 출력되지 않는다.
+
+if (name1 === "TOM" || isAdult == true) {
+  // 이름이 TOM이거나 성인인 경우 > 통과
+  console.log("PASS");
+} // 조건 1개 만족 > PASS 출력
+
+const name2 = "Mike";
+const age2 = 30;
+
+if (name2 === "TOM" || age2 > 19) {
+  // false || true > true
+  console.log("PASS");
+} else {
+  console.log("NOT PASS");
+} // PASS 출력
+
+if (name2 === "TOM" && age2 > 19) {
+  // false && true
+  console.log("PASS");
+} else {
+  console.log("NOT PASS");
+} // NOT PASS 출력
+
+if (name2 === "TOM" || age2 < 11) {
+  // false || false
+  console.log("PASS");
+} else {
+  console.log("NOT PASS");
+} // NOT PASS
